7d8b9dca830efa7c3c65347855db8232537c70e0
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2011-08-30 23:06:52 +0300

    Commit des fichiers de Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMSRoR::Application.config.secret_token = '673a4f1a8387525466cc309fe74a7046972e132a591c4182b93e68f10ba9b1948a3768bf3930e3290ce7739bb983ee190f1eace7f3afb93d880beab5d8257a83'
