e2e0c7c9673a8bfad6437b35a362e523efd83cc5
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2011-09-07 07:57:55 +0300

    Génération des fichiers controlleur + modele pour User

--- lib/generators/cms/cms_generator.rb
+++ lib/generators/cms/cms_generator.rb
@@ -1,3 +1,9 @@
 class CmsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  #User related files
+  def copy_user_files
+    copy_file "users_controller.rb", "app/controllers/users_controller.rb"
+    copy_file "user.rb", "app/models/user.rb"
+  end
 end
